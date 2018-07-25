25d93709f02694ae00af96e86a92b6a7302ccab7
Bruno Rocha <rochacbruno@gmail.com>
2018-07-25 14:43:28 -0300

    No unnecessary memory allocations

--- list.rs
+++ list.rs
@@ -2,19 +2,21 @@ use std::fs;
 use std::path::Path;
 
 fn render(image_path: &str) -> String {
-    let s: String = format!(
+    format!(
         "https://github.com/rochacbruno/rust_memes/blob/master/{image_path}
 ![https://raw.githubusercontent.com/rochacbruno/rust_memes/master/{image_path}](https://raw.githubusercontent.com/rochacbruno/rust_memes/master/{image_path})
 ---",
         image_path = image_path
-    );
-    s.trim().to_string()
+    ).trim().into()
 }
 
 fn main() {
     let path = Path::new("img");
     for entry in fs::read_dir(path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
-        println!("{}", render(entry.path().to_str().expect("Error to convert path")));
+        println!(
+            "{}",
+            render(entry.path().to_str().expect("Error to convert path"))
+        );
     }
 }
